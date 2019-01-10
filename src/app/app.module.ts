aa56dc0d8ce8b593d9bdf093c634e7649f48ada5
Andreeva Anastasia <aam.fdev@gmail.com>
2019-01-10 18:51:35 +0300

    some fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NestedComponent } from './nested-component/nested.component';
+import { NestedComponent } from './nested/nested.component';
+import { ProjectionComponent } from './projection/projection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NestedComponent
+    NestedComponent,
+    ProjectionComponent
   ],
   imports: [
     BrowserModule,
