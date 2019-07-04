16bd954048a05102d46a0f0df9395ac54c5a47cc
Andreeva Anastasia <aam.fdev@gmail.com>
2019-07-04 11:56:22 +0300

    corrected app structure

--- src/app/directives/colory.directive.ts
+++ src/app/directives/colory.directive.ts
@@ -1,12 +1,19 @@
 import { Directive, HostBinding, HostListener } from '@angular/core';
 
+// декоратор директивы включает параметр selector (атрибут селектор)
 @Directive({
   selector: '[appColory]',
   exportAs: 'colory'
 })
 export class ColoryDirective {
 
+  // декораторы для воздействия на элемент
+  // HostBinding - служит для обновления свойств эелемента, на который повесили
+  // HostListener - служит для обработки событий
   @HostBinding('style.color') myColor: string;
+
+  // первый параметр событие
+  // второй необязательный но может сказать что хотим получить событие event
   @HostListener('click', ['$event']) changeColor(event) {
     // this.myColor = 'green';
     this.setRandomColor();
